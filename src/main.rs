e2c191abd3f316d4249ec43dd404eb006b334000
William W Wold <wilcoze@gmail.com>
2017-06-19 03:08:32 -0700

    split up big function

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,35 @@ enum Set {
 	And(Vec<Set>),
 }
 
+fn is_val_in_set(val: f64, set: &Set) -> bool {
+	match set {
+		&Set::Is(super_val) => {
+			return val == super_val;
+		},
+		&Set::Or(ref super_children) => {
+			for super_child in super_children {
+				if is_val_in_set(val, super_child) {
+					return true;
+				}
+			}
+			return false;
+		},
+		&Set::And(ref super_children) => {
+			for super_child in super_children {
+				if !is_val_in_set(val, super_child) {
+					return false;
+				}
+			}
+			return true;
+		},
+	}
+}
+
 fn is_subset(sub_set: &Set, super_set: &Set) -> bool {
 	
 	match sub_set {
-		&Set::Is(ref sub_val) => {
-			match super_set {
-				&Set::Is(ref super_val) => {
-					return sub_val == super_val;
-				},
-				&Set::Or(ref super_children) => {
-					for super_child in super_children {
-						if is_subset(sub_set, super_child) {
-							return true;
-						}
-					}
-					return false;
-				},
-				&Set::And(ref super_children) => {
-					for super_child in super_children {
-						if !is_subset(sub_set, super_child) {
-							return false;
-						}
-					}
-					return true;
-				},
-			}
+		&Set::Is(sub_val) => {
+			return is_val_in_set(sub_val, super_set);
 		},
 		&Set::Or(ref sub_children) => {
 			for sub_child in sub_children {
